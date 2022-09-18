27f0ffe945999c6c07e2b07fd1a83dda167b86e8
oksana-1 <4438488@gmail.com>
2022-09-18 15:51:11 +0300

    Add comments, results, make code more understandable, change line separator to `LF`

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const addItems = (num) => {
-	const rootElem = document.querySelector("#wrapper");
-	for (let i = 0; i < num; i++) {
-		const item = document.createElement("div");
-		item.classList.add("item");
-		item.innerHTML = `Item #${i + 1}`
-		rootElem.append(item);
-	}
-}
+/**
+ * Returns milliseconds passed from start to end of a function
+ *
+ * @param {function} fn Function to measure performance
+ * @param {number} limit The number of times to run a function
+ * @return {number} average number of milliseconds to execute 1 function call
+ */
+
 const measure = (fn, limit) => {
 	const before = performance.now();
 	for (let i = 0; i < limit; i++) {
@@ -14,10 +13,20 @@ const measure = (fn, limit) => {
 	}
 	return (performance.now() - before) / limit;
 }
+// Measuring performance of :first-child, :last-child selectors
+const addItems = (num) => {
+	const rootElem = document.querySelector("#firstLastSelectorWrapper");
+	for (let i = 0; i < num; i++) {
+		const item = document.createElement("div");
+		item.classList.add("item");
+		item.innerHTML = `Item #${i + 1}`
+		rootElem.append(item);
+	}
+}
 const measureSelectors = () => {
-	const firstChild = measure(() => document.querySelector("#wrapper .item:first-child"), 5000);
-	const lastChild = measure(() => document.querySelector("#wrapper .item:last-child"), 5000);
-	const element = document.querySelector("#summary");
+	const firstChild = measure(() => document.querySelector("#firstLastSelectorWrapper .item:first-child"), 5000);
+	const lastChild = measure(() => document.querySelector("#firstLastSelectorWrapper .item:last-child"), 5000);
+	const element = document.querySelector("#firstLastSelectorSummary");
 	element.innerHTML = `
 		<p>Get first child took: ${firstChild} ms</p>
 		<p>Get last child took: ${lastChild} ms</p>
@@ -25,10 +34,17 @@ const measureSelectors = () => {
 }
 addItems(100);
 measureSelectors();
+
+// Measuring performance of fibonacci on web-workers
 const runFibonacciOnWorkers = () => {
+	const result = document.querySelector("#fibonacciWebWorkersPerformance");
 	const workerCode = document.querySelector("#workerCode").textContent;
 	const blob = new Blob([workerCode], {type: "text/javascript"});
 	const url = URL.createObjectURL(blob);
-	new Worker(url);
+	const fibonacciWorker = new Worker(url);
+	// Receive value from web-worker
+	fibonacciWorker.onmessage = function(e) {
+		result.innerHTML = `<p>Everage run of fibonacci-fn took: ${e.data} ms</p>`;
+	}
 }
 runFibonacciOnWorkers();
