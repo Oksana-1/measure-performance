1b51f0a156ebf134a6da7c2bd4bb9be21cd88a84
oksana-1 <4438488@gmail.com>
2023-08-07 13:25:47 +0300

    Array.includes vs Object.hasOwnProperty

--- index.js
+++ index.js
@@ -82,3 +82,65 @@ element.innerHTML = `
 }
 measurePromisesAndObservables();
 
+// Measuring performance of array.includes vs object.hasOwnProperty
+function generateArray(limit) {
+	const array = [];
+	setTimeout(() => {
+		for (let i = 1; i < limit; i++) {
+			array.push(`Value-${i}`);
+		}
+	});
+	return array;
+}
+function generateObject(limit) {
+	const obj = {};
+	setTimeout(() => {
+		for (let i = 1; i < limit; i++) {
+			obj[`Value-${i}`] = true;
+		}
+	});
+	return obj;
+}
+
+
+setTimeout(() => {
+	measureArrayGenerator();
+}, 0);
+setTimeout(() => {
+	measureObjectGenerator();
+}, 0);
+setTimeout(() => {
+	measureArrayVsObjects(5000)
+}, 0)
+function measureArrayGenerator() {
+	const generateArrayTime = measure(() => generateArray(5000), 5000);
+	const element = document.getElementById("generateArray");
+	element.innerHTML = `It took ${generateArrayTime} ms to generate array with 5000 elements`
+}
+function measureObjectGenerator() {
+	const generateObjTime = measure(() => generateArray(5000), 5000);
+	const element = document.getElementById("generateObject");
+	element.innerHTML = `It took ${generateObjTime} ms to generate object with 5000 elements`
+}
+function measureArrayVsObjects (times) {
+	const array =  generateArray(5000);
+	const obj = generateObject(5000);
+	function findInArray(value) {
+		return array.includes(value);
+	}
+	function findInObject (value) {
+		return obj.hasOwnProperty(value);
+	}
+	const arrayIncludesTimeTruthy = measure(() => findInArray("Value-1"), times);
+	const arrayIncludesTimeFalsy = measure(() => findInArray("Value-0"), times);
+	const objectHasOwnPropertyTimeTruthy = measure(() => findInObject("Value-1"), times);
+	const objectHasOwnPropertyTimeFalsy = measure(() => findInObject("Value-0"), times);
+	const arrayIncludesTruthy = document.getElementById("arrayIncludesTruthy");
+	const arrayIncludesFalsy = document.getElementById("arrayIncludesFalsy");
+	const objectHasOwnPropertyTruthy = document.getElementById("objectHasOwnPropertyTruthy");
+	const objectHasOwnPropertyFalsy = document.getElementById("objectHasOwnPropertyFalsy");
+	arrayIncludesTruthy.innerHTML = `It took ${arrayIncludesTimeTruthy} ms to find is element in array (for truthy search)`;
+	arrayIncludesFalsy.innerHTML =  `It took ${arrayIncludesTimeFalsy} ms to find is element in array (for falsy search)`;
+	objectHasOwnPropertyTruthy.innerHTML = `It took ${objectHasOwnPropertyTimeTruthy} ms to find is element in object (for truthy search)`;
+	objectHasOwnPropertyFalsy.innerHTML = `It took ${objectHasOwnPropertyTimeFalsy} ms to find is element in object (for falsy search)`;
+}
